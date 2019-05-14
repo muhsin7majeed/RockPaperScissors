a187975cd77e42dafbd9b7e2fd07b625691d712e
Muhsin <muhsin7ak@gmail.com>
2019-05-14 13:18:02 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,3 @@
+I made this by following DevEd on youtube.
+you can find his video here ;
+https://www.youtube.com/watch?v=qWPtKtYEsN4&list=PLDyQo7g0_nsX8_gZAB8KD1lL4j4halQBJ&index=10
---
+++ app.js
@@ -0,0 +1,91 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+
+    const startGame = () => {
+        const playButton = document.querySelector(".intro button");
+        const introScreen = document.querySelector(".intro");
+        const match = document.querySelector(".match");
+
+        playButton.addEventListener("click", () => {
+            introScreen.classList.add("fadeOut");
+            match.classList.add("fadeIn");
+        });
+    };
+    const playMatch = () => {
+        const options = document.querySelectorAll(".options button");
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
+
+        const computerOptions = ["rock", "paper", "scissors"];
+
+        options.forEach(option => {
+            option.addEventListener("click", function() {
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoise = computerOptions[computerNumber];
+
+                compareHands(this.textContent, computerChoise);
+
+                playerHand.src = `./assets/${this.textContent}.png`;
+                computerHand.src = `./assets/${computerChoise}.png`;
+            });
+        });
+    };
+    const updateScore = () => {
+        const playerScore = document.querySelector(".player-score p");
+        const computerScore = document.querySelector(".computer-score p");
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+    };
+    const compareHands = (playerChoice, computerChoice) => {
+        const winner = document.querySelector(".winner");
+        if (playerChoice === computerChoice) {
+            winner.textContent = "It is a tie";
+            return;
+        }
+        if (playerChoice === "rock") {
+            if (computerChoice === "scissors") {
+                winner.textContent = "Player Wins";
+                pScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = "Computer Wins";
+                cScore++;
+                updateScore();
+                return;
+            }
+        }
+        if (playerChoice === "paper") {
+            if (computerChoice === "scissors") {
+                winner.textContent = "Computer Wins";
+                cScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = "Player Wins";
+                cScore++;
+                updateScore();
+                return;
+            }
+        }
+        if (playerChoice === "scissors") {
+            if (computerChoice === "rock") {
+                winner.textContent = "Computer Wins";
+                cScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = "Player Wins";
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
+    };
+
+    startGame();
+    playMatch();
+};
+
+game();
